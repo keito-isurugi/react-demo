d8a9373dc164fe9dd4ed12254f55385028436b91
Isurugi Keito <62861107+keito-isurugi@users.noreply.github.com>
2022-07-16 08:15:24 +0900

    再レンダリング

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, {useState} from "react"
-import {ChildArea} from "./ChildArea"
+import React, { useState, useCallback } from "react";
+import { ChildArea } from "./ChildArea";
 
 import "./styles.css";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [open, setOpen] = useState(false);
 
   const onChangeText = (e) => {
-    setText(e.target.value)
-  }
+    setText(e.target.value);
+  };
   const onClickOpen = () => {
     setOpen(!open);
-  }
-
+  };
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
 
   return (
     <div className="App">
-      <input value={text} onChange={onChangeText}/>
+      <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open}/>
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,24 +1,27 @@
+import { memo } from "react";
+
 const style = {
   width: "100%",
   height: "200px",
   backgroundColor: "aqua"
-}
+};
 
-export const ChildArea = (props) =>  {
-  const {open} = props
-  console.log('ChildAreaがレンダリング')
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+  console.log("ChildAreaがレンダリング");
 
-  const data = [...Array(2000).keys()]
+  const data = [...Array(100).keys()];
   data.forEach(() => {
-    console.log('...')
-  })
+    console.log("...");
+  });
   return (
     <>
       {open ? (
-        <div  style={style}>
+        <div style={style}>
           <p>child components</p>
+          <button onClick={onClickClose}>close</button>
         </div>
       ) : null}
     </>
-  )
-}
+  );
+});
