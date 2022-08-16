3f3c3fb5da7ff008c4e6fa9fa68140d4518f2c92
KeitoIsurugi <isurugi@csplan.jp>
2022-08-16 18:40:48 +0900

    react-router navlink

--- src/demo/router/Header.jsx
+++ src/demo/router/Header.jsx
@@ -1,13 +1,21 @@
 import { BrowserRouter, Route, Routes, Link, NavLink, useResolvedPath, useMatch, useNavigate, Outlet } from "react-router-dom"
+import styled from '@emotion/styled'
+import './router.css'
 
 export const Header = () => {
   return (
     <>
-      <Link to="/home">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/dropzone">Dropzone</Link>
-      <Link to="/pagination">Pagination</Link>
-      <Link to="/chakra_responsive">chakra_responsive</Link>
+      <SNavLink to="/home">Home</SNavLink>
+      <SNavLink to="/about">About</SNavLink>
+      <SNavLink to="/dropzone">Dropzone</SNavLink>
+      <SNavLink to="/pagination">Pagination</SNavLink>
+      <SNavLink to="/chakra_responsive">chakra_responsive</SNavLink>
     </>
   )
 }
+
+const SNavLink = styled(NavLink)`
+  font-weight: bold;
+  margin-right: 10px;
+  padding: 10px;
+`
