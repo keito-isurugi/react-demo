b7ebdf6a66c72773688e4d0a1faaf4b013bd1937
KeitoIsurugi <ja224kg@gmail.com>
2022-09-16 10:01:20 +0900

    型変換について

--- src/demo/router/Home.jsx
+++ src/demo/router/Home.jsx
@@ -1,8 +1,21 @@
 export const Home = () => {
-  console.log('home')
+  
+  const str = 'Hello'
+  const num = 123
+
   return (
     <>
       <h1>Home</h1>
+      <span>{str}</span><span>：{typeof(str)}</span><br/>
+      <span>{str}</span><span>：Numberに変換→{Number(str)}</span><br/>
+      <span>{num}</span><span>：{typeof(num)}</span><br/>
+      <span>{num}</span><span>：Stringに変換→{String(num)}</span><br/>
+      <span>Number("Hello") === "NaN" ? "true": "false"</span><span>→{Number("Hello") === "NaN" ? "true": "false"}</span><br/>
+      <span>→必ずfalseが返される</span><br/>
+      <span>isNaN(Number("Hello")) ? "true": "false"</span><span>→{isNaN(Number("Hello")) ? "true": "false"}</span><br/>
+      <span>→isNaNを使うとNaNを条件分岐に使える</span><br/>
+      <span>isNaN(Number(123)) ? "true": "false"</span><span>→{isNaN(Number(123)) ? "true": "false"}</span><br/>
+      <span>→数値だとfalseが返される</span><br/>
     </>
   );
 };
